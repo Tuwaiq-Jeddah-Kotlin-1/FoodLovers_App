ae3fcd4581f169c1c7b576d64110ee594b9d5bf8
RanaJaberAlrajhi <Rana.Alrajhi.jed.k1@tuwaiq.edu.sa>
2021-12-29 17:14:13 +0300

    sharedPreference

--- app/src/main/java/com/example/foodloverscapston2/view/MainActivity.kt
+++ app/src/main/java/com/example/foodloverscapston2/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.foodloverscapston2.view
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,12 +8,19 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.foodloverscapston2.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var sher = this.getSharedPreferences("share_pref", MODE_PRIVATE)
+        var lang = sher.getString("share", "")
+       lang.let{
+        setLocale(lang.toString())
+       }
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
@@ -25,6 +33,18 @@ class MainActivity : AppCompatActivity() {
                 else -> bottomNavView.visibility = View.VISIBLE
             }
         }
+    }
+    fun setLocale(lang: String){
+        val locale = Locale(lang)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.locale = locale
+        this.resources.updateConfiguration(config,this.resources.displayMetrics)
+
+        var sharedPreferences = this.getSharedPreferences("share_pref", MODE_PRIVATE)
+        var editor = sharedPreferences.edit()
+        editor.putString("share_pref", lang)
+        editor.commit()
     }
 }
 
